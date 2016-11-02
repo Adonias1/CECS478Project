84b7c5722668b7ee51002c4962c71b4c6d64ee0c
hennvsty <nuggetsasukegirl@gmail.com>
2016-11-02 11:27:43 -0700

    Our Login and Register

---
+++ Login.php
@@ -0,0 +1,72 @@
+<?php
+use \Firebase\JWT\JWT; 
+//require_once 'Connect.php';
+$servername = "";
+$myDBusername = "";
+$myDBpassword = "";
+	
+try {
+    $conn = mysqli_connect($servername, $myDBusername, $myDBpassword, "");
+    // set the PDO error mode to exception
+    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+	else{
+		echo "Connected successfully <br/>";
+	}
+     
+    }
+catch(Exception $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+require_once 'JWT.php';
+define('SECRET_KEY','');
+define('ALGORITHM','HS512');   
+  
+                // if there is no error below code run
+global $username, $password;
+$username = $_POST['username'];
+$password = $_POST['password'];
+$statement = mysqli_prepare($conn, "select id, username, password from users where username = ?" );
+mysqli_stmt_bind_param($statement, "s", $username);
+mysqli_stmt_execute($statement);
+mysqli_stmt_bind_result($statement, $id, $username, $passwordHash);
+$row = mysqli_stmt_fetch($statement);
+if(count($row)>0 && password_verify($password,$passwordHash)){
+	$tokenId    = base64_encode(mcrypt_create_iv(32));
+	$issuedAt   = time();
+	$notBefore  = $issuedAt + 10;  //Adding 10 seconds
+	$expire     = $notBefore + 7200; // Adding 60 seconds
+	$serverName = 'localhost'; /// set your domain name 
+	/*
+	* Create the token as an array
+	*/
+	$data = [
+		'iat'  => $issuedAt,         // Issued at: time when the token was generated
+		'jti'  => $tokenId,          // Json Token Id: an unique identifier for the token
+		'iss'  => $serverName,       // Issuer
+		'nbf'  => $notBefore,        // Not before
+		'exp'  => $expire,           // Expire
+		'data' => [                  // Data related to the logged user you can set your required data
+		// id from the users table
+			'id' => $id,
+			'name' => $username, //  name
+		]	
+    ];
+	$secretKey = base64_decode(SECRET_KEY);
+	/// Here we will transform this array into JWT:
+	$jwt = JWT::encode(
+	$data, //Data to be encoded in the JWT
+	$secretKey, // The signing key
+	ALGORITHM 
+	); 
+	$unencodedArray = ['jwt' => $jwt];
+	echo  "{'status' : 'success','resp':".json_encode($unencodedArray)."}";
+} else {
+	echo  "{'status' : 'error','msg':'Invalid email or password'}";
+}
+    
+?>     
---
+++ Register.php
@@ -0,0 +1,60 @@
+<?php
+    
+    $servername = "";
+	$myDBusername = "";
+	$myDBpassword = "";
+
+try {
+    $conn = mysqli_connect($servername, $myDBusername, $myDBpassword, "");
+	if (mysqli_connect_errno()) {
+		echo "Connection failed<br/>";
+	}
+	else{
+		echo "Connected successfully<br/>";
+	}	 
+} catch(Exception $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+	
+function registerUser() {
+	global $conn, $username, $password;
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+	try{
+        $statement = mysqli_prepare($conn, "INSERT INTO users (username, password) VALUES (?, ?)");
+        mysqli_stmt_bind_param($statement, "ss", $username, $passwordHash);
+        mysqli_stmt_execute($statement);
+		echo mysqli_stmt_error($statement);
+        mysqli_stmt_close($statement);
+	}catch(Exception $e){
+		echo "could not run statement<br/>";
+	}
+		
+}
+function usernameAvailable() {
+	global $conn, $username;
+	$username = $_POST['username'];
+    $statement = mysqli_prepare($conn, "SELECT username FROM users WHERE username = ?");
+	mysqli_stmt_bind_param($statement, "s", $username);
+	mysqli_stmt_execute($statement);
+	mysqli_stmt_bind_result($statement, $username);
+	$count = mysqli_stmt_fetch($statement);
+	//mysqli_stmt_close($statement);
+	if (!(count($count) > 0)){
+		return true;
+    }else {
+		return false;
+    }
+}
+$response = array();
+$response["success"] = false;
+if (usernameAvailable()){
+	registerUser();
+	$response["success"] = true;
+}
+else{
+	echo "username already exists<br/>";
+}
+echo json_encode($response);
+?>
